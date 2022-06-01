1659914ed39caea920f2cc21aebb46318c0ab5aa
Bow Vernon <bowvernon110391@gmail.com>
2022-06-01 20:34:14 +0700

    initial commit

---
+++ b2dtest.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+An attempt at some simple, self-contained pygame-based examples.
+Example 02
+
+In short:
+One static body:
+    + One fixture: big polygon to represent the ground
+Two dynamic bodies:
+    + One fixture: a polygon
+    + One fixture: a circle
+And some drawing code that extends the shape classes.
+
+kne
+"""
+import pygame
+from pygame.locals import (QUIT, KEYDOWN, K_ESCAPE, KEYUP, K_a, K_d)
+
+import Box2D  # The main library
+# Box2D.b2 maps Box2D.b2Vec2 to vec2 (and so on)
+from Box2D.b2 import (world, polygonShape, circleShape, staticBody, dynamicBody)
+
+# --- constants ---
+# Box2D deals with meters, but we want to display pixels,
+# so define a conversion factor:
+PPM = 20.0  # pixels per meter
+TARGET_FPS = 60
+TIME_STEP = 1.0 / TARGET_FPS
+SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
+
+# --- pygame setup ---
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
+pygame.display.set_caption('Simple pygame example')
+clock = pygame.time.Clock()
+
+# --- pybox2d world setup ---
+# Create the world
+world = world(gravity=(0, -10), doSleep=True)
+
+# And a static body to hold the ground shape
+ground_body = world.CreateStaticBody(
+    position=(0, 0),
+    shapes=polygonShape(box=(50, 1)),
+)
+ground_body.fixtures[0].friction=0.9
+
+# Create a couple dynamic bodies
+body = world.CreateDynamicBody(position=(1, 15))
+circle = body.CreateCircleFixture(radius=0.5, density=1, friction=0.9)
+circle.filterData.groupIndex=-2
+# body.ApplyLinearImpulse(Box2D.b2Vec2(5, 0), body.transform * Box2D.b2Vec2_zero, True)
+body.linearVelocity = Box2D.b2Vec2(10.0, -15.0)
+
+tire = body
+# print(circle)
+
+# second tire (slippery)
+body = world.CreateDynamicBody(position=(1, 15))
+circle2 = body.CreateCircleFixture(radius=0.5, density=1, friction=0.0)
+circle2.filterData.groupIndex=-2
+body.linearVelocity = Box2D.b2Vec2(10.0, -15.0)
+
+tire2 = body
+
+# a box angled
+body = world.CreateDynamicBody(position=(30, 45), angle=15)
+box = body.CreatePolygonFixture(box=(2, 1), density=1, friction=0.3)
+
+# a bigger box on the left
+body = world.CreateDynamicBody(position=(3, 5), angle=0)
+box = body.CreatePolygonFixture(box=(1, 2), density=1, friction=0.3)
+
+colors = {
+    staticBody: (255, 255, 255, 255),
+    dynamicBody: (127, 127, 127, 255),
+}
+
+# Let's play with extending the shape classes to draw for us.
+
+
+def my_draw_polygon(polygon, body, fixture):
+    vertices = [(body.transform * v) * PPM for v in polygon.vertices]
+    vertices = [(v[0], SCREEN_HEIGHT - v[1]) for v in vertices]
+    pygame.draw.polygon(screen, colors[body.type], vertices)
+polygonShape.draw = my_draw_polygon
+
+
+def my_draw_circle(circle, body, fixture):
+    position = body.transform * circle.pos * PPM
+    position = (position[0], SCREEN_HEIGHT - position[1])
+    pygame.draw.circle(screen, colors[body.type], [int(
+        x) for x in position], int(circle.radius * PPM))
+
+    # line_end = body.GetWorldPoint(Box2D.b2Vec2(circle.radius * PPM, 0.0))
+
+    # print(f"{position[0]:.2f}, {position[1]:.2f} | {line_end.x:.2f}, {line_end.y:.2f}")
+    line_end = body.transform * Box2D.b2Vec2(circle.radius, 0) * PPM
+    line_end = (line_end[0], SCREEN_HEIGHT - line_end[1])
+    
+    pygame.draw.line(screen, colors[0], [int(x) for x in position], [int(x) for x in line_end])
+    # Note: Python 3.x will enforce that pygame get the integers it requests,
+    #       and it will not convert from float.
+circleShape.draw = my_draw_circle
+
+# --- main game loop ---
+
+torque_mult = 0
+
+running = True
+while running:
+    # Check the event queue
+    for event in pygame.event.get():
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+            # The user closed the window or pressed escape
+            running = False
+
+        if event.type == KEYDOWN and event.key == K_d:
+            torque_mult -= 1
+        if event.type == KEYUP and event.key == K_d:
+            torque_mult += 1
+
+        if event.type == KEYDOWN and event.key == K_a:
+            torque_mult += 1
+        if event.type == KEYUP and event.key == K_a:
+            torque_mult -= 1
+    
+    tire.ApplyTorque(torque_mult * 5.0, True)
+
+    # pygame.display.set_caption(f'{tire.}')
+    pygame.display.set_caption(f'tire vel: {tire.linearVelocity.x:.2f}, {tire.linearVelocity.y:.2f}')
+
+    screen.fill((0, 0, 0, 0))
+    # Draw the world
+    for body in world.bodies:
+        for fixture in body.fixtures:
+            fixture.shape.draw(body, fixture)
+
+    # Make Box2D simulate the physics of our world for one step.
+    world.Step(TIME_STEP, 10, 10)
+
+    # Flip the screen and try to keep at the target FPS
+    pygame.display.flip()
+    clock.tick(TARGET_FPS)
+
+pygame.quit()
+print('Done!')
